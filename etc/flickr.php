4216684643bcd6986c080e89ec45bd11c56b5f01
alberto2112 <alberto2112@users.noreply.github.com>
2015-05-05 11:34:14 +0200

    Delete flickr.php

--- etc/flickr.php
+++
@@ -1,5 +0,0 @@
-<?php
-    define('FLICKR_KEY', '0d4bb41df8b86c27cf53a6cd802fbaf2');
-    define('FLICKR_SECRET','75423f21fdc90cc4');
-    define('FLICKR_TOKEN', '72157651816877919-203f2dd316498360');
-?>
